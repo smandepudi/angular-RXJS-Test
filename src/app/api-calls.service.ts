0ef470de7b8e8c9f13cc4c7f719dbfe3e834b991
smandepudi <sindhura.ull@gmail.com>
2022-08-25 21:08:16 -0700

    1 file modified

--- src/app/api-calls.service.ts
+++ src/app/api-calls.service.ts
@@ -94,22 +94,101 @@ export class ApiCallsService {
     return this.getAll().pipe(
       mergeMap((result: any) => {
         let r = [...result];
-        console.log('ids', result);
-        let allAuthors = r.map((book) => this.getBookAuthor(book.bookId));
-        let allTitles = r.map((book) => this.getBookTitle(book.bookId));
+
+        // console.log('ids', result);
+        // let allAuthors = r.map((book) => this.getBookAuthor(book.bookId));
+        // let allTitles = r.map((book) => this.getBookTitle(book.bookId));
 
         /* comment 2 */
 
         /*  I was only able to get authors and add it to the object but not titles */
-        return forkJoin(...allAuthors).pipe(
-          map((authorArray) => {
-            console.log('Author Array', authorArray);
-            r.forEach((eachAuthor, index) => {
-              eachAuthor.author = authorArray[index].author;
-            });
-            return r;
-          })
-        );
+        // return forkJoin(...allAuthors).pipe(
+        //   map((authorArray) => {
+        //     console.log('Author Array', authorArray);
+        //     let authorArr = [...result];
+        //     authorArr.forEach((eachAuthor, index) => {
+        //       eachAuthor.author = authorArray[index].author;
+        //     });
+        //     return r;
+        //   })
+        // );
+
+        // return forkJoin(
+        //   this.getBookAuthor('0001'),
+        //   this.getBookTitle('0001')
+        //   // getMultiValueObservable(), forkJoin on works for observables that complete
+        // ).pipe(
+        //   map(([first, second]) => {
+        //     // forkJoin returns an array of values, here we map those values to an object
+        //     // return { first, second };
+        //     console.log(first);
+        //     console.log(second);
+        //     r.forEach((orgArr, index) => {
+        //       orgArr.author = first.author;
+        //       orgArr.title = second.title;
+        //     });
+        //     return r;
+        //   })
+        // );
+
+        // const requests = r.map((item) => {
+        //   return forkJoin(
+        //     this.getBookAuthor(item.bookId),
+        //     this.getBookTitle(item.bookId)
+        //     // getMultiValueObservable(), forkJoin on works for observables that complete
+        //   ).pipe(
+        //     map(([first, second]) => {
+        //       // return {
+        //       //   originalItem: item,
+        //       //   requestResult: result
+        //       // };
+        //       console.log(first);
+        //       console.log(second);
+        //       return [];
+        //     })
+        //   );
+        // });
+
+        /* working better but can only get author and not Title*/
+        const requests = r.map((item) => {
+          return this.getBookAuthor(item.bookId).pipe(
+            map((result) => {
+              return {
+                bookId: item.bookId,
+                author: result.author,
+              };
+            })
+          );
+        });
+
+        return forkJoin(requests);
+
+        /* working better */
+        // const authorRequests = r.map((item) => {
+        //   return this.getBookAuthor(item.bookId).pipe(
+        //     map((result) => {
+        //       return {
+        //         bookId: item.bookId,
+        //         author: result.author,
+        //       };
+        //     })
+        //   );
+        // });
+
+        // const titleRequests = r.map((item) => {
+        //   return this.getBookTitle(item.bookId).pipe(
+        //     map((result) => {
+        //       return {
+        //         bookId: item.bookId,
+        //         author: result.title,
+        //       };
+        //     })
+        //   );
+        // });
+
+        // return forkJoin(authorRequests, titleRequests);
+
+        // return r;
       })
     );
   }
